983fa301fa2d36f83824396dd09c663c9b5d3705
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2024-01-26 20:38:22 +0200

    init

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,31 @@
 const express = require('express')
 const router = express.Router()
 
+// Тут ввиводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  //res.render генерує нам HTML сторінку
+
+  // сюди вводимо назву файлу з container
+  res.render('index', {
+    //   вказуємо назву контейнера
+    name: 'index',
+    //    вказуємо назву компонентів
+    component: [''],
+    // вказуємо назву сторінки
+    title: 'Home page',
+
+    //   ... сюди можна продовжувати добавляти потрібні технічні дані
+    data: {},
+  })
+  // сюди виводимо JSON дані
+})
+
 // Підключіть файли роутів
-const test = require('./test')
+const auth = require('./auth')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use('/', auth)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
